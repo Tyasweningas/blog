68b48b4a2fe74ff1733905c9436fa55e056afa8b
Tyasweningas <tyasweningas@gmail.com>
2026-01-18 10:35:55 +0700

    feat(talent-growth): add commennt routes

--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const {
+    updateComment,
+    deleteComment
+} = require('../controllers/comment.controller');
+const auth = require('../middleware/auth.middleware');
+const { authorizeComment } = require('../middleware/authorize.middleware');
+
 const router = express.Router();
 
-// TODO: Implement CRUD controllers
-router.get('/', (req, res) => res.json({ message: 'Comments endpoint' }));
+router.put('/:id', auth, authorizeComment, updateComment);
+router.delete('/:id', auth, authorizeComment, deleteComment);
 
 module.exports = router;

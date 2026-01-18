c9b8c2a1f56da48fbe201376b55a8c196639f944
Tyasweningas <tyasweningas@gmail.com>
2026-01-18 09:45:55 +0700

    feat(talent-growth): model for post

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        comments: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment',
+        }],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model('Post', postSchema);
